c5a07c8fb98e70b9ee06e747ad26d081e702cd22
brahmaCapgemini <brahmanand.gorati@capgemini.com>
2023-08-30 12:56:21 +0530

    Implemented Infinite

--- base-infinite-swapi/src/people/InfinitePeople.jsx
+++ base-infinite-swapi/src/people/InfinitePeople.jsx
@@ -1,4 +1,5 @@
 import InfiniteScroll from "react-infinite-scroller";
+import { useInfiniteQuery } from 'react-query';
 import { Person } from "./Person";
 
 const initialUrl = "https://swapi.dev/api/people/";
@@ -8,6 +9,30 @@ const fetchUrl = async (url) => {
 };
 
 export function InfinitePeople() {
+  const { data, fetchNextPage, hasNextPage, isLoading, isFetched, isError, error } = useInfiniteQuery(
+    'sw-people',
+    ({ pageParam = initialUrl }) => fetchUrl(pageParam),
+    {
+      getNextPageParam: (lastPage) => lastPage.next || undefined
+    }
+  );
+  if (isFetched) return <div>Loading...</div>;
+  if (isError) return <div>Error! {error.toString()}</div>;
+
+
   // TODO: get data for InfiniteScroll via React Query
-  return <InfiniteScroll />;
+  return <InfiniteScroll loadMore={fetchNextPage} hasMore={hasNextPage}>
+    {data.pages.map((pageData) => {
+      return pageData.results.map((persons) => {
+        return (
+          <Person
+            key={persons.name}
+            name={persons.name}
+            hairColor={persons.hair_color}
+            eyeColor={persons.eye_color}
+          />
+        )
+      })
+    })}
+  </InfiniteScroll>;
 }
